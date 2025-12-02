8c56e983a436c0093c54151e06e05ecd58ffe5dc
Ryan H. <77473341+ryanhammer@users.noreply.github.com>
2025-12-02 13:21:41 -0500

    Update part_1 with corrected solution

--- day-2/part_1.tsx
+++ day-2/part_1.tsx
@@ -1,26 +1,47 @@
+const exampleRanges = [
+  "11-22",
+  "95-115",
+  "998-1012",
+  "1188511880-1188511890",
+  "222220-222224",
+  "1698522-1698528",
+  "446443-446449",
+  "38593856-38593862",
+  "565653-565659",
+  "824824821-824824827",
+  "2121212118-2121212124",
+];
+
 const idRanges = ["24-46","124420-259708","584447-720297","51051-105889","6868562486-6868811237","55-116","895924-1049139","307156-347325","372342678-372437056","1791-5048","3172595555-3172666604","866800081-866923262","5446793-5524858","6077-10442","419-818","57540345-57638189","2143479-2274980","683602048-683810921","966-1697","56537997-56591017","1084127-1135835","1-14","2318887654-2318959425","1919154462-1919225485","351261-558210","769193-807148","4355566991-4355749498","809094-894510","11116-39985","9898980197-9898998927","99828221-99856128","9706624-9874989","119-335"];
 
-function determineInvalidIds (idRanges: string[]): number {
-    let invalidIdSum = 0;
+function determineInvalidIdSum (idRanges: string[]): number {
+  let invalidIdSum = 0;
+  let invalidIdCount = 0;
 
-    idRanges.forEach((range) => {
-        const rangeParts = range.split("-");
-        const lowerLimit = Number(rangeParts[0]);
-        const upperLimit = Number(rangeParts[1]);
+  idRanges.forEach((range) => {
+    const rangeParts = range.split("-");
 
-        const startValue = Number(rangeParts[0].slice(0, Math.floor(rangeParts[0].length / 2)));
-        const endValue = Number(rangeParts[1].slice(0, Math.floor(rangeParts[1].length / 2)));
+    const lowerLimit = Number(rangeParts[0]);
+    const upperLimit = Number(rangeParts[1]);
 
-        for (let i = startValue; i<=endValue; i++) {
-            const valueToTest = Number(i.toString() + i.toString());
-            
-            if (valueToTest >= lowerLimit && valueToTest <= upperLimit) {
-                invalidIdSum += valueToTest;
-            }
-        }
-    })
+    for (let i = lowerLimit; i<=upperLimit; i++) {
+      const numberStringLength = i.toString().length;
+      if (numberStringLength % 2 !== 0) continue;
 
-    return invalidIdSum;
+      const half = numberStringLength / 2;
+      const firstHalf = Number(i.toString().slice(0, half));
+      const secondHalf = Number(i.toString().slice(half));
+      
+      if (firstHalf === secondHalf) {
+          invalidIdSum += i;
+          invalidIdCount++;
+      }
+    }
+  })
+
+  console.log(invalidIdCount);
+  return invalidIdSum;
 }
 
-console.log(determineInvalidIds(idRanges));
+console.log(determineInvalidIdSum(exampleRanges));
+console.log(determineInvalidIdSum(idRanges));
